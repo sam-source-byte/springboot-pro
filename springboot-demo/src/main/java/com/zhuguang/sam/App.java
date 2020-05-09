9c2ba1a18d87a8b6724163de85beb4762174dc61
sam-source-byte <1908656945@qq.com>
2020-05-09 20:51:11 +0800

    Springboot练习

--- springboot-demo/src/main/java/com/zhuguang/sam/App.java
+++ springboot-demo/src/main/java/com/zhuguang/sam/App.java
@@ -3,6 +3,8 @@ package com.zhuguang.sam;
 /**
  * IDEA上传项目到GitHub仓库详细步骤
  * https://blog.csdn.net/jesseakylin/article/details/96120600
+ *IDEA发布项目到Github，修改文件后更新GitHub中的项目具体操作
+ * https://blog.csdn.net/m0_43584016/article/details/88544877
  *
  */
 public class App 
