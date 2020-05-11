907d5075af108e3ad8b526a836d88d00fb638fe1
sam-source-byte <1908656945@qq.com>
2020-05-11 12:14:21 +0800

    加入数据库连接池代码

--- springboot-demo/src/main/java/com/zhuguang/sam/config/JdbcConfig.java
+++ springboot-demo/src/main/java/com/zhuguang/sam/config/JdbcConfig.java
@@ -1,6 +1,7 @@
 package com.zhuguang.sam.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +14,21 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class JdbcConfig {
+    @Value("${jdbc.url}")
+    String url;
+    @Value("${jdbc.driverClassName}")
+    String driverClassName;
+    @Value("${jdbc.username}")
+    String username;
+    @Value("${jdbc.password}")
+    String password;
     @Bean
     public DataSource dataSource(){
         DruidDataSource druidDataSource = new DruidDataSource();
-      /*  druidDataSource.setDriverClassName(driverClassName);
+       druidDataSource.setDriverClassName(driverClassName);
         druidDataSource.setUrl(url);
         druidDataSource.setUsername(username);
-        druidDataSource.setPassword(password);*/
+        druidDataSource.setPassword(password);
         return druidDataSource;
     }
 }
--- springboot-demo/src/main/java/com/zhuguang/sam/controller/HelloController.java
+++ springboot-demo/src/main/java/com/zhuguang/sam/controller/HelloController.java
@@ -1,10 +1,13 @@
 package com.zhuguang.sam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.sql.DataSource;
 
 /**
  *   @Controller
@@ -16,10 +19,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class HelloController    {
+    @Autowired
+    private DataSource dataSource;
     //http://localhost:8080/hello
     @RequestMapping("/hello")
     public  String hello(){
-        return "hello,Spring Boot!";
+        return "hello,Spring Boot!"+dataSource;
     }
 
 }
